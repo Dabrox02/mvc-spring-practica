4abf2b24b9f658d15d5a38da1ad3dad4af41dd5f
Dabrox02 <jaiderstyvenmendoza@gmail.com>
2023-08-19 21:11:45 -0500

    Se simula persistencia de datos con una clase

--- src/main/java/co/edu/uts/appdev/controllers/IndexController.java
+++ src/main/java/co/edu/uts/appdev/controllers/IndexController.java
@@ -2,13 +2,29 @@ package co.edu.uts.appdev.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import co.edu.uts.appdev.models.Usuario;
 import org.springframework.ui.Model;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
+
     @GetMapping({"/index", "/", "home", ""})
     public String index(Model model){
         model.addAttribute("titulo", "Hola Spring Framework");
-        return "index.html";
+        return "index";
+    }
+
+    @GetMapping("/perfil")
+    public String perfil(Model model){
+        model.addAttribute("titulo", "Perfil Usuario");
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Jaider");
+        usuario.setApellido("Mendoza");
+        usuario.setEmail("jaider@gmail.com");
+        model.addAttribute("usuario", usuario);
+        return "perfil";
     }
 }
---
+++ src/main/java/co/edu/uts/appdev/models/Usuario.java
@@ -0,0 +1,17 @@
+package co.edu.uts.appdev.models;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    public Usuario(String nombre, String apellido, String email){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email =  email;
+    }
+}
